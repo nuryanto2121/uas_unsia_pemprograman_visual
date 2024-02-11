4ffbe7368316b6bc01441d53bc269ae33cd3297a
nuryanto <nuryantofattih@gmail.com>
2024-02-11 19:51:51 +0700

    [add]initiate project uas Pemprograman Visual Unsia

---
+++ Form1.Designer.vb
@@ -0,0 +1,340 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        lblNim = New Label()
+        lblNama = New Label()
+        lblJenisKelamin = New Label()
+        lblTempat = New Label()
+        lblTglLahir = New Label()
+        lblAlamat = New Label()
+        txtNim = New TextBox()
+        txtNama = New TextBox()
+        cbJenisKelamin = New ComboBox()
+        txtTempat = New TextBox()
+        dtpTglLahir = New DateTimePicker()
+        txtAlamat = New TextBox()
+        btnImport = New Button()
+        btnEdit = New Button()
+        btnHapus = New Button()
+        btnKeluar = New Button()
+        btnSimpan = New Button()
+        OpenFileDialog1 = New OpenFileDialog()
+        ListView1 = New ListView()
+        txtNilai = New TextBox()
+        Label2 = New Label()
+        Label3 = New Label()
+        cbJurusan = New ComboBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Arial Narrow", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(214, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(175, 31)
+        Label1.TabIndex = 0
+        Label1.Text = "Data Mahasiswa"
+        ' 
+        ' lblNim
+        ' 
+        lblNim.AutoSize = True
+        lblNim.Font = New Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        lblNim.Location = New Point(13, 83)
+        lblNim.Name = "lblNim"
+        lblNim.Size = New Size(32, 20)
+        lblNim.TabIndex = 2
+        lblNim.Text = "NIM"
+        ' 
+        ' lblNama
+        ' 
+        lblNama.AutoSize = True
+        lblNama.Font = New Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        lblNama.Location = New Point(12, 53)
+        lblNama.Name = "lblNama"
+        lblNama.Size = New Size(43, 20)
+        lblNama.TabIndex = 3
+        lblNama.Text = "Nama"
+        ' 
+        ' lblJenisKelamin
+        ' 
+        lblJenisKelamin.AutoSize = True
+        lblJenisKelamin.Font = New Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        lblJenisKelamin.Location = New Point(12, 112)
+        lblJenisKelamin.Name = "lblJenisKelamin"
+        lblJenisKelamin.Size = New Size(93, 20)
+        lblJenisKelamin.TabIndex = 4
+        lblJenisKelamin.Text = "Jenis Kelamin"
+        ' 
+        ' lblTempat
+        ' 
+        lblTempat.AutoSize = True
+        lblTempat.Font = New Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        lblTempat.Location = New Point(12, 143)
+        lblTempat.Name = "lblTempat"
+        lblTempat.Size = New Size(53, 20)
+        lblTempat.TabIndex = 5
+        lblTempat.Text = "Tempat"
+        ' 
+        ' lblTglLahir
+        ' 
+        lblTglLahir.AutoSize = True
+        lblTglLahir.Font = New Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        lblTglLahir.Location = New Point(12, 173)
+        lblTglLahir.Name = "lblTglLahir"
+        lblTglLahir.Size = New Size(59, 20)
+        lblTglLahir.TabIndex = 6
+        lblTglLahir.Text = "Tgl Lahir"
+        ' 
+        ' lblAlamat
+        ' 
+        lblAlamat.AutoSize = True
+        lblAlamat.Font = New Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        lblAlamat.Location = New Point(12, 204)
+        lblAlamat.Name = "lblAlamat"
+        lblAlamat.Size = New Size(49, 20)
+        lblAlamat.TabIndex = 7
+        lblAlamat.Text = "Alamat"
+        ' 
+        ' txtNim
+        ' 
+        txtNim.BorderStyle = BorderStyle.FixedSingle
+        txtNim.Location = New Point(191, 84)
+        txtNim.Name = "txtNim"
+        txtNim.Size = New Size(199, 23)
+        txtNim.TabIndex = 9
+        ' 
+        ' txtNama
+        ' 
+        txtNama.BorderStyle = BorderStyle.FixedSingle
+        txtNama.Location = New Point(190, 54)
+        txtNama.Name = "txtNama"
+        txtNama.Size = New Size(199, 23)
+        txtNama.TabIndex = 8
+        ' 
+        ' cbJenisKelamin
+        ' 
+        cbJenisKelamin.FormattingEnabled = True
+        cbJenisKelamin.Items.AddRange(New Object() {"Laki-Laki", "Perempuan"})
+        cbJenisKelamin.Location = New Point(190, 113)
+        cbJenisKelamin.Name = "cbJenisKelamin"
+        cbJenisKelamin.Size = New Size(199, 23)
+        cbJenisKelamin.TabIndex = 10
+        ' 
+        ' txtTempat
+        ' 
+        txtTempat.BorderStyle = BorderStyle.FixedSingle
+        txtTempat.Location = New Point(190, 142)
+        txtTempat.Name = "txtTempat"
+        txtTempat.Size = New Size(199, 23)
+        txtTempat.TabIndex = 11
+        ' 
+        ' dtpTglLahir
+        ' 
+        dtpTglLahir.CustomFormat = "yyyy/MM/dd"
+        dtpTglLahir.Format = DateTimePickerFormat.Custom
+        dtpTglLahir.Location = New Point(190, 172)
+        dtpTglLahir.Name = "dtpTglLahir"
+        dtpTglLahir.Size = New Size(199, 23)
+        dtpTglLahir.TabIndex = 12
+        ' 
+        ' txtAlamat
+        ' 
+        txtAlamat.BorderStyle = BorderStyle.FixedSingle
+        txtAlamat.Location = New Point(190, 201)
+        txtAlamat.Multiline = True
+        txtAlamat.Name = "txtAlamat"
+        txtAlamat.Size = New Size(199, 99)
+        txtAlamat.TabIndex = 13
+        ' 
+        ' btnImport
+        ' 
+        btnImport.Location = New Point(12, 321)
+        btnImport.Name = "btnImport"
+        btnImport.Size = New Size(75, 23)
+        btnImport.TabIndex = 16
+        btnImport.Text = "&Import"
+        btnImport.UseVisualStyleBackColor = True
+        ' 
+        ' btnEdit
+        ' 
+        btnEdit.Location = New Point(197, 321)
+        btnEdit.Name = "btnEdit"
+        btnEdit.Size = New Size(75, 23)
+        btnEdit.TabIndex = 18
+        btnEdit.Text = "&Edit"
+        btnEdit.UseVisualStyleBackColor = True
+        ' 
+        ' btnHapus
+        ' 
+        btnHapus.Location = New Point(383, 321)
+        btnHapus.Name = "btnHapus"
+        btnHapus.Size = New Size(75, 23)
+        btnHapus.TabIndex = 19
+        btnHapus.Text = "&Hapus"
+        btnHapus.UseVisualStyleBackColor = True
+        ' 
+        ' btnKeluar
+        ' 
+        btnKeluar.Location = New Point(484, 321)
+        btnKeluar.Name = "btnKeluar"
+        btnKeluar.Size = New Size(75, 23)
+        btnKeluar.TabIndex = 20
+        btnKeluar.Text = "&Keluar"
+        btnKeluar.UseVisualStyleBackColor = True
+        ' 
+        ' btnSimpan
+        ' 
+        btnSimpan.Location = New Point(104, 321)
+        btnSimpan.Name = "btnSimpan"
+        btnSimpan.Size = New Size(75, 23)
+        btnSimpan.TabIndex = 17
+        btnSimpan.Text = "&Simpan"
+        btnSimpan.UseVisualStyleBackColor = True
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "OpenFileDialog1"
+        ' 
+        ' ListView1
+        ' 
+        ListView1.Font = New Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        ListView1.GridLines = True
+        ListView1.Location = New Point(12, 362)
+        ListView1.Name = "ListView1"
+        ListView1.Size = New Size(899, 152)
+        ListView1.TabIndex = 21
+        ListView1.UseCompatibleStateImageBehavior = False
+        ListView1.View = View.Details
+        ' 
+        ' txtNilai
+        ' 
+        txtNilai.BorderStyle = BorderStyle.FixedSingle
+        txtNilai.Location = New Point(600, 85)
+        txtNilai.Name = "txtNilai"
+        txtNilai.Size = New Size(199, 23)
+        txtNilai.TabIndex = 15
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(422, 84)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(34, 20)
+        Label2.TabIndex = 22
+        Label2.Text = "Nilai"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label3.Location = New Point(422, 53)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(55, 20)
+        Label3.TabIndex = 21
+        Label3.Text = "Jurusan"
+        ' 
+        ' cbJurusan
+        ' 
+        cbJurusan.FormattingEnabled = True
+        cbJurusan.Items.AddRange(New Object() {"PJJ - Informatika", "PJJ - System informatika", "PJJ - Management", "PJJ - Komunikasi", "PJJ - Akuntansi"})
+        cbJurusan.Location = New Point(600, 53)
+        cbJurusan.Name = "cbJurusan"
+        cbJurusan.Size = New Size(199, 23)
+        cbJurusan.TabIndex = 14
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(291, 321)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 23
+        Button1.Text = "&Edit"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(923, 598)
+        Controls.Add(Button1)
+        Controls.Add(cbJurusan)
+        Controls.Add(txtNilai)
+        Controls.Add(Label2)
+        Controls.Add(Label3)
+        Controls.Add(ListView1)
+        Controls.Add(btnSimpan)
+        Controls.Add(btnKeluar)
+        Controls.Add(btnHapus)
+        Controls.Add(btnEdit)
+        Controls.Add(btnImport)
+        Controls.Add(txtAlamat)
+        Controls.Add(dtpTglLahir)
+        Controls.Add(txtTempat)
+        Controls.Add(cbJenisKelamin)
+        Controls.Add(txtNama)
+        Controls.Add(txtNim)
+        Controls.Add(lblAlamat)
+        Controls.Add(lblTglLahir)
+        Controls.Add(lblTempat)
+        Controls.Add(lblJenisKelamin)
+        Controls.Add(lblNama)
+        Controls.Add(lblNim)
+        Controls.Add(Label1)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "UAS Pemprograman Visual"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblNim As Label
+    Friend WithEvents lblNama As Label
+    Friend WithEvents lblJenisKelamin As Label
+    Friend WithEvents lblTempat As Label
+    Friend WithEvents lblTglLahir As Label
+    Friend WithEvents lblAlamat As Label
+    Friend WithEvents txtNim As TextBox
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents cbJenisKelamin As ComboBox
+    Friend WithEvents txtTempat As TextBox
+    Friend WithEvents dtpTglLahir As DateTimePicker
+    Friend WithEvents txtAlamat As TextBox
+    Friend WithEvents btnImport As Button
+    Friend WithEvents btnEdit As Button
+    Friend WithEvents btnHapus As Button
+    Friend WithEvents btnKeluar As Button
+    Friend WithEvents btnSimpan As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents txtNilai As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents cbJurusan As ComboBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,177 @@
+﻿Imports Scripting
+Imports Microsoft.VisualBasic.FileIO
+
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call setHeaderListView()
+    End Sub
+
+    Private Sub btnImport_Click(sender As Object, e As EventArgs) Handles btnImport.Click
+        Dim ofd As New OpenFileDialog
+        ofd.Filter = "CSV Files|*.csv"
+        ofd.FilterIndex = 1
+        ofd.Multiselect = False
+
+        If ofd.ShowDialog() = vbOK Then
+            Dim selectedFilePath As String
+            selectedFilePath = ofd.FileName
+            'call procedur membaca file csv
+            Call readCsvFileN(selectedFilePath)
+        End If
+    End Sub
+
+    Sub setHeaderListView()
+        ListView1.Columns.Add("Nama", 150)
+        ListView1.Columns.Add("NIM", 100)
+        ListView1.Columns.Add("Jenis Kelamin", 110)
+        ListView1.Columns.Add("Tempat", 80)
+        ListView1.Columns.Add("Tgl Lahir", 100)
+        ListView1.Columns.Add("Alamat", 150)
+        ListView1.Columns.Add("Jurusan", 150)
+        ListView1.Columns.Add("Nilai", 50)
+        ListView1.Columns.Add("Status", 100)
+
+
+    End Sub
+    Sub simpanMahasiswa(nim As String, nama As String, jenisKelamin As String, tempat As String, tglLahir As String, alamat As String, jurusan As String, nilai As Int32)
+        Dim newItems As New ListViewItem(nama)
+        newItems.SubItems.Add(nim)
+        newItems.SubItems.Add(jenisKelamin)
+        newItems.SubItems.Add(tempat)
+        newItems.SubItems.Add(tglLahir)
+        newItems.SubItems.Add(alamat)
+        newItems.SubItems.Add(jurusan)
+        newItems.SubItems.Add(nilai)
+        If nilai >= 70 Then
+            newItems.SubItems.Add("Lulus")
+        Else
+            newItems.SubItems.Add("Tidak Lulus")
+        End If
+        ListView1.Items.Add(newItems)
+
+    End Sub
+
+    Private Sub btnSimpan_Click(sender As Object, e As EventArgs) Handles btnSimpan.Click
+        'validasi nim dan nama tidak boleh kosong
+        If txtNim.Text = "" Or txtNama.Text = "" Then
+            MessageBox.Show("NIM atau Nama tidak boleh kosong!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+        Call simpanMahasiswa(txtNim.Text, txtNama.Text, cbJenisKelamin.Text, txtTempat.Text, dtpTglLahir.Value.ToString(), txtAlamat.Text, cbJurusan.Text, txtNilai.Text)
+        Call clearInput()
+
+
+    End Sub
+    Private Sub clearInput()
+        txtNim.Text = ""
+        txtNama.Text = ""
+        cbJenisKelamin.Text = ""
+        txtTempat.Text = ""
+        dtpTglLahir.Value = Now()
+        txtAlamat.Text = ""
+        cbJurusan.Text = ""
+        txtNilai.Text = ""
+    End Sub
+
+    Private Sub ListView1_Click(sender As Object, e As EventArgs) Handles ListView1.Click
+        If ListView1.SelectedItems.Count > 0 Then
+            txtNama.Text = ListView1.SelectedItems(0).SubItems(0).Text
+            txtNim.Text = ListView1.SelectedItems(0).SubItems(1).Text
+            cbJenisKelamin.Text = ListView1.SelectedItems(0).SubItems(2).Text
+            txtTempat.Text = ListView1.SelectedItems(0).SubItems(3).Text
+            Dim tglLahir As String = ListView1.SelectedItems(0).SubItems(4).Text
+            txtAlamat.Text = ListView1.SelectedItems(0).SubItems(5).Text
+            dtpTglLahir.Value = CDate(tglLahir)
+            cbJurusan.Text = ListView1.SelectedItems(0).SubItems(6).Text
+            txtNilai.Text = ListView1.SelectedItems(0).SubItems(7).Text
+
+        End If
+    End Sub
+
+    Private Sub btnHapus_Click(sender As Object, e As EventArgs) Handles btnHapus.Click
+        If ListView1.SelectedItems.Count > 0 Then
+            Dim result As DialogResult = MessageBox.Show("Yakin Anda akan menghapus data ini?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+            If result = DialogResult.Yes Then
+                ListView1.Items.Remove(ListView1.SelectedItems(0))
+                Call clearInput()
+            End If
+
+        End If
+    End Sub
+
+    Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+        If ListView1.SelectedItems.Count > 0 Then
+            ListView1.SelectedItems(0).SubItems(0).Text = txtNama.Text
+            ListView1.SelectedItems(0).SubItems(1).Text = txtNim.Text
+            ListView1.SelectedItems(0).SubItems(2).Text = cbJenisKelamin.Text
+            ListView1.SelectedItems(0).SubItems(3).Text = txtTempat.Text
+            ListView1.SelectedItems(0).SubItems(4).Text = dtpTglLahir.Value
+            ListView1.SelectedItems(0).SubItems(5).Text = txtAlamat.Text
+            ListView1.SelectedItems(0).SubItems(6).Text = cbJurusan.Text
+            ListView1.SelectedItems(0).SubItems(7).Text = txtNilai.Text
+            If txtNilai.Text >= 70 Then
+                ListView1.SelectedItems(0).SubItems(8).Text = "Lulus"
+            Else
+                ListView1.SelectedItems(0).SubItems(8).Text = "Lulus""Tidak Lulus"
+            End If
+
+        End If
+        Call clearInput()
+    End Sub
+
+    Private Sub btnKeluar_Click(sender As Object, e As EventArgs) Handles btnKeluar.Click
+        ' Menampilkan MessageBox konfirmasi sebelum menutup aplikasi
+        Dim result As DialogResult = MessageBox.Show("Yakin Anda akan keluar dari aplikasi?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If result = DialogResult.Yes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Call clearInput()
+    End Sub
+    Private Sub readCsvFileN(pathFile As String)
+        Using MyReader As New TextFieldParser(pathFile)
+            MyReader.TextFieldType = FileIO.FieldType.Delimited
+            MyReader.SetDelimiters(",")
+            Dim i As Long = 0
+
+            Dim currentRow As String()
+            While Not MyReader.EndOfData
+                Try
+                    currentRow = MyReader.ReadFields()
+
+                    'eksekusi selain header
+                    If i > 0 Then
+                        ' Menambahkan data mahasiswa dengan memanggail procedur simpanMahasiswa
+                        Dim nim, nama, jenisKelamin, tempatLahir, tglLahir, alamat, jurusan As String
+                        Dim nilai As Int32
+
+                        nama = currentRow(0)
+                        nim = currentRow(1)
+                        jenisKelamin = currentRow(2)
+                        tempatLahir = currentRow(3)
+                        tglLahir = currentRow(4)
+                        alamat = currentRow(5)
+                        jurusan = currentRow(6)
+                        nilai = currentRow(7)
+
+                        'call procedur simpan data mahasiswa
+                        Call simpanMahasiswa(nim, nama, jenisKelamin, tempatLahir, tglLahir, alamat, jurusan, nilai)
+                    End If
+
+
+                    i += 1
+                Catch ex As MalformedLineException
+                    MsgBox("Line " & ex.Message &
+                    "is not valid and will be skipped.")
+                End Try
+            End While
+        End Using
+    End Sub
+
+
+End Class
